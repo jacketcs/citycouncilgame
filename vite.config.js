b72527d190410e4f05c32786548cd4812ed44382
shoshanaokeefe <72363379+shoshanaokeefe@users.noreply.github.com>
2025-06-15 21:19:11 -1000

    fix vite.config error

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/citycouncilgame/'
   plugins: [react()],
+  base:'/citycouncilgame/'
 
 })
